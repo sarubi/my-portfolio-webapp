c13e9089c232532c9392c0667e047da928d2fcbb
Nipun Theekshana <theekshana2@gmail.com>
2023-04-01 13:03:17 +0530

    change webGL geomatry

--- src/components/dev-profile/Tagline.tsx
+++ src/components/dev-profile/Tagline.tsx
@@ -1,7 +1,7 @@
 const Tagline: React.FC = () => {
 
   return (
-    <div className="tagline mt-[50px] text-center bg-[#F5F0E8] dark:bg-[#313134] p-5 rounded-[10px]">
+    <div className="tagline mt-[20px] text-center bg-[#F5F0E8] dark:bg-[#313134] p-5 rounded-[10px]">
       As a web3 software architect and developer, I revel in constructing innovative blockchain solutions that revolutionize the digital landscape. Crafting the future, one block at a time.
     </div>
   )
